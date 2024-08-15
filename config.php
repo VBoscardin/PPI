865efe3ce7d69054f762bf3a784e3dd289ce99cc
VBoscardin <165020041+VBoscardin@users.noreply.github.com>
2024-08-15 11:51:46 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "bd_ppi";
-$username = "root";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
